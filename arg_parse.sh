1e92f3b8b5fe8dcc088b4119f181a42bc4aa7c97
kkdd <kkondo_z0@nifty.com>
2026-04-30 06:27:00 +0900

    Refactor argument parsing and improve help messages

--- arg_parse.sh
+++ arg_parse.sh
@@ -3,9 +3,10 @@
 # constants
 TRUE="true"
 NINDENT=2
-VALUE_IN_SAME=0
-VALUE_IN_NEXT=1
-EXTRACT_FAILED=2  # for the case of invalid or missing value in parsing
+VALUE_IN_SAME=0  # extract value from a connected form (-n1, --num1)
+VALUE_IN_NEXT=1  # extract value from an unconnected form (-n 1, --num 1)
+EXTRACT_FAILED=2 # extract failed (missing or invalid value)
+UNDEFINED_VALUE="UNDEFINED VALUE"  # used in the case of extract failed
 
 # default values (long option names are used as the corresponding variable names)
 loops_var=1  # the suffix of _var is appended for variables
@@ -13,16 +14,18 @@ loops_var=1  # the suffix of _var is appended for variables
 warning=""
 missing=""
 
+# command requirements
 commands_required="fmt"
 commands_gnu=""  # GNU commands required
 
+# messages
 PROGNAME=${0##*/}
 USAGE="Usage:
   $PROGNAME [options] [ARGUMENT...]"
 OPTIONS="Options:
   -h|--help          Show this usage and exit.
   -d|--debug         Enable debug mode.
-  -l|--loops N       Set the number of loops (default: $loops_var)
+  -l|--loops N       Set the number of loops (default: $loops_var).
   -m|--mammal NAME   Set mammal name.
   -S                 Enable s-flag.
   -T T               Set t-value."
@@ -30,15 +33,14 @@ DESCRIPTION="Description:
 Parse command-line arguments."
 EXAMPLES="Examples:
   dash $PROGNAME --debug --loops=2 --mammal=cat,dog I II III"
-WARNING_HEAD="Warning: Type \"$PROGNAME --help\" for usage instructions."
+WARNING_HEAD="**Warning:** Type \"$PROGNAME --help\" for usage."
 
 # functions
 append_warning_f() {
   [ -z "$warning" ] && warning="$1" || warning="$warning$(printf '\n%s' "$1")"
 }
 
-# Extract optional argument value from the connected forms such as -n1, --num1, -n=1, or --num=1 using option prefix tokens -n--num asigned by $1.
-extract_opt_value_in_same_f() {
+extract_opt_value_in_same_f() {  # Extract optional argument value from the connected forms such as -n1, --num1, -n=1, or --num=1 using option prefix tokens -n--num asigned by $1.
   opt_value="$2"  # initial value (from current argument)
   prefix_tokens="$(printf '%s' "$1" | sed 's/\([^ -]\)-/\1 -/g')"  # split tokens by inserting a space before '-'.
   prefix_tokens="$prefix_tokens ="  # append "=" as a delimiter for stripping
@@ -48,32 +50,34 @@ extract_opt_value_in_same_f() {
   printf '%s' "$opt_value"
 }
 
-# Extract optional argument value futhermore from the unconnected forms such as -n 1, --num 1, -n= 1, or --num= 1 also using option prefix tokens -n--num asigned by $1.
-extract_opt_value_f() {
+extract_opt_value_f() {  # Extract optional argument value futhermore from the unconnected forms such as -n 1, --num 1, -n= 1, or --num= 1 also using option prefix tokens -n--num asigned by $1.
   opt_value="$(extract_opt_value_in_same_f "$1" "$2")"  # default at start
   value_from=$VALUE_IN_SAME
   if [ -z "$opt_value" ]; then
     opt_value="$3"  # the value may reside at the next place
     value_from=$VALUE_IN_NEXT
-    case "$opt_value" in
-      -* | "" )  # failed
-        opt_value=""
-        value_from=$EXTRACT_FAILED;;
-    esac
+    if [ "${3+z}" != z ]; then
+      opt_value="$UNDEFINED_VALUE"; value_from=$EXTRACT_FAILED
+    else
+      case "$opt_value" in
+        -*)  opt_value="$UNDEFINED_VALUE"; value_from=$EXTRACT_FAILED ;;
+      esac
+    fi
   fi
   printf '%s' "$opt_value"
-  return $value_from  # caller will use it as an additional shift unless failed
+  return $value_from  # caller will use it as an additional shift except failed
 }
 
-# parsing optional arguments
+# parse optional arguments
+ # shellcheck disable=SC2034  # variables unused
 while [ $# -gt 0 ]; do
-  case "$1" in
+   case "$1" in
     -h|--help)      help_enabled=$TRUE;;
     -d|--debug)     debug_enabled=$TRUE;;
     -l*|--loops*)   loops_var=$(extract_opt_value_f -l--loops "$@");;
     -m*|--mammal*)  mammal_var=$(extract_opt_value_f -m--mammal "$@");;
     -S)             s_enabled=$TRUE;;
-    -T*)            t_var=$(extract_opt_value_f -t "$@");;
+    -T*)            t_var=$(extract_opt_value_f -T "$@");;
     -[!-][!-]*)     # split combined short options: -abc → -a -bc
                     rest_flags=${1#??}; first_flag=${1%"$rest_flags"}; shift;
                     set -- "$first_flag" "-$rest_flags" "$@"; continue;;
@@ -83,7 +87,7 @@ while [ $# -gt 0 ]; do
   esac
   shift_additional=$?
   if [ $shift_additional -lt $EXTRACT_FAILED ]; then
-    shift $shift_additional  # 0: default, 1: extract_opt_value_f has successfully taken value from next arg
+    shift $shift_additional  # 0: default, 1: extract_opt_value_f successfully extracts the next arg
   else
     append_warning_f "Option '${1%=}' requires an argument"
   fi
@@ -92,7 +96,7 @@ done
 
 # confirmations
 for e in "$@"; do
-  case "$e" in -*) append_warning_f "Each argument should not begin with \"-\": '$e'";; esac
+  case "$e" in -*) append_warning_f "Argument value should not begin with \"-\": '$e'";; esac
 done
 
 case "$(uname -s)" in
@@ -114,35 +118,46 @@ done
 [ -n "$missing" ] && append_warning_f "Required Command: $missing"
 
 # warn or show usage instruction
+text_style_f() { ESC=$(printf '\033'); printf "${ESC}[%dm" "$1"; }
+NORMALFACE="$(text_style_f 0)" 
 if [ -n "$help_enabled" ]; then
   printf '%s\n' "$USAGE"
   if [ -n "$DESCRIPTION" ]; then
     format_text_block_f() {
       fmt_f() {
-        LINE_WIDTH=80  # the traditional  width
+        LINE_WIDTH=80  # the traditional column width
         w="-w$((LINE_WIDTH - 1))"  # option for fmt
-        fmt "$w" </dev/null >/dev/null 2>&1 || w=""  # dry run for the option
+        fmt "$w" </dev/null >/dev/null 2>&1 || unset w  # dry run for the option
         fmt "$w"
       }
       awk -v n="$NINDENT" '/^[A-Z].*:$/ {print; next}{printf "%*s%s\n", n, "", $0}' | fmt_f
     }
     printf '\n%s\n' "$DESCRIPTION" | format_text_block_f
   fi
-  [ -n "$OPTIONS" ] && printf '\n%s\n' "$OPTIONS"
+  if [ -n "$OPTIONS" ]; then
+    decorate_f() {
+      [ -t 1 ] || { cat; return; }  # stop decoration
+      GREEN="$(text_style_f 36)" BAR="$GREEN|$NORMALFACE" UNDERLINE="$(text_style_f 4)"
+      sed "s/|/$BAR/g" |
+      sed "s/\( \)\([A-Z][A-Z]*\)\([^ A-Z]*\)\(  \)/\1${UNDERLINE}\2${NORMALFACE}\3\4/"  # underline uppercase variable names
+    }
+    printf '\n%s\n' "$OPTIONS" | decorate_f
+  fi
   [ -n "$EXAMPLES" ] && printf '\n%s\n' "$EXAMPLES"
   if [ -n "$commands_required" ]; then
-    commands_required_="$(printf '%s' "$commands_required" | sed "s/ /, /g")"
-    printf '\n%s\n' "Required commands: $commands_required_"
+    s="$(printf '%s' "$commands_required" | sed "s/ /, /g")"
+    printf '\n%s\n' "Required commands: $s"
   fi
-  exit
-elif [ -n "$warning" ]; then
-  text_style_f() { ESC=$(printf '\033'); printf "${ESC}[%dm" "$1"; }
-  NORMALFACE="$(text_style_f 0)"; MAGENTA="$(text_style_f 31)"
-  printf '%s\n' "$WARNING_HEAD" |
-    awk -v m="$MAGENTA" -v n="$NORMALFACE" 'sub(/^Warning:/, m "&" n)' >&2
+fi
+if [ -n "$warning" ]; then
+  magenta_f() {  #  emphasize **strings** as colored magenta
+    MAGENTA="$(text_style_f 31)"
+    sed "s/\*\*\([^_]*\)\*\*/${MAGENTA}\1${NORMALFACE}/g"
+  }
+  printf '%s\n' "$WARNING_HEAD" | magenta_f  >&2
   printf '%s\n' "$warning" | sed 's/^/- /' >&2
   exit 1
 fi
+[ -n "$help_enabled" ] && exit
 
 # main
-# for e in "$@"; do printf "%s\n" "$e"; done
